52b31a265ca29750729d7af954bfd80993caca03
Soufian Jebbara <s.jebbara@gmail.com>
2017-10-04 14:59:04 +0200

    update to newest keras with python 2.7

--- src/custom_layers.py
+++ src/custom_layers.py
@@ -1,169 +1,192 @@
-import numpy as np
 
 from keras import backend as K
-from keras.engine.topology import Layer
 from keras.engine import InputSpec
 from keras.layers import Recurrent, ELU, Wrapper
-from keras import activations, initializations, regularizers, constraints
-from keras.layers.recurrent import time_distributed_dense
+from keras import activations, initializers, regularizers, constraints
+from keras.layers.recurrent import _time_distributed_dense
+from keras.legacy import interfaces
+
+
+def _elu(x):
+    return K.elu(x, 1.0)
 
 
 class ELUGRU(Recurrent):
-    def __init__(self, output_dim, init='glorot_uniform', inner_init='orthogonal', inner_activation='hard_sigmoid',
-                 W_regularizer=None, U_regularizer=None, b_regularizer=None, dropout_W=0., dropout_U=0., **kwargs):
-        self.output_dim = output_dim
-        self.inner_init = initializations.get(inner_init)
-        self.inner_activation = activations.get(inner_activation)
-        self.W_regularizer = regularizers.get(W_regularizer)
-        self.U_regularizer = regularizers.get(U_regularizer)
-        self.b_regularizer = regularizers.get(b_regularizer)
-        self.dropout_W = dropout_W
-        self.dropout_U = dropout_U
-
-        self.init = "he_normal"
-        self.activation = lambda x: K.elu(x, 1.0)
-
-        if self.dropout_W or self.dropout_U:
-            self.uses_learning_phase = True
+    def __init__(self, units, recurrent_activation='hard_sigmoid', use_bias=True, kernel_initializer='he_normal',
+                 recurrent_initializer='orthogonal', bias_initializer='zeros', kernel_regularizer=None,
+                 recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None,
+                 recurrent_constraint=None, bias_constraint=None, dropout=0., recurrent_dropout=0., **kwargs):
         super(ELUGRU, self).__init__(**kwargs)
+        self.units = units
+        self.activation = _elu
+        self.recurrent_activation = activations.get(recurrent_activation)
+        self.use_bias = use_bias
+
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.recurrent_initializer = initializers.get(recurrent_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.recurrent_regularizer = regularizers.get(recurrent_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.recurrent_constraint = constraints.get(recurrent_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+
+        self.dropout = min(1., max(0., dropout))
+        self.recurrent_dropout = min(1., max(0., recurrent_dropout))
+        self.state_spec = InputSpec(shape=(None, self.units))
 
     def build(self, input_shape):
-        self.input_spec = [InputSpec(shape=input_shape)]
-        self.input_dim = input_shape[2]
+        if isinstance(input_shape, list):
+            input_shape = input_shape[0]
 
+        batch_size = input_shape[0] if self.stateful else None
+        self.input_dim = input_shape[2]
+        self.input_spec[0] = InputSpec(shape=(batch_size, None, self.input_dim))
+
+        self.states = [None]
         if self.stateful:
             self.reset_states()
-        else:
-            # initial states: all-zero tensor of shape (output_dim)
-            self.states = [None]
 
-        if self.consume_less == 'gpu':
-            self.W = self.add_weight((self.input_dim, 3 * self.output_dim), initializer=self.init,
-                                     name='{}_W'.format(self.name), regularizer=self.W_regularizer)
-            self.U = self.add_weight((self.output_dim, 3 * self.output_dim), initializer=self.inner_init,
-                                     name='{}_U'.format(self.name), regularizer=self.U_regularizer)
-            self.b = self.add_weight((self.output_dim * 3,), initializer='zero', name='{}_b'.format(self.name),
-                                     regularizer=self.b_regularizer)
-        else:
-            self.W_z = self.add_weight((self.input_dim, self.output_dim), initializer=self.init,
-                                       name='{}_W_z'.format(self.name), regularizer=self.W_regularizer)
-            self.U_z = self.add_weight((self.output_dim, self.output_dim), initializer=self.init,
-                                       name='{}_U_z'.format(self.name), regularizer=self.W_regularizer)
-            self.b_z = self.add_weight((self.output_dim,), initializer='zero', name='{}_b_z'.format(self.name),
-                                       regularizer=self.b_regularizer)
-            self.W_r = self.add_weight((self.input_dim, self.output_dim), initializer=self.init,
-                                       name='{}_W_r'.format(self.name), regularizer=self.W_regularizer)
-            self.U_r = self.add_weight((self.output_dim, self.output_dim), initializer=self.init,
-                                       name='{}_U_r'.format(self.name), regularizer=self.W_regularizer)
-            self.b_r = self.add_weight((self.output_dim,), initializer='zero', name='{}_b_r'.format(self.name),
-                                       regularizer=self.b_regularizer)
-            self.W_h = self.add_weight((self.input_dim, self.output_dim), initializer=self.init,
-                                       name='{}_W_h'.format(self.name), regularizer=self.W_regularizer)
-            self.U_h = self.add_weight((self.output_dim, self.output_dim), initializer=self.init,
-                                       name='{}_U_h'.format(self.name), regularizer=self.W_regularizer)
-            self.b_h = self.add_weight((self.output_dim,), initializer='zero', name='{}_b_h'.format(self.name),
-                                       regularizer=self.b_regularizer)
-            self.W = K.concatenate([self.W_z, self.W_r, self.W_h])
-            self.U = K.concatenate([self.U_z, self.U_r, self.U_h])
-            self.b = K.concatenate([self.b_z, self.b_r, self.b_h])
+        self.kernel = self.add_weight(shape=(self.input_dim, self.units * 3), name='kernel',
+                                      initializer=self.kernel_initializer, regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        self.recurrent_kernel = self.add_weight(shape=(self.units, self.units * 3), name='recurrent_kernel',
+                                                initializer=self.recurrent_initializer,
+                                                regularizer=self.recurrent_regularizer,
+                                                constraint=self.recurrent_constraint)
 
-        if self.initial_weights is not None:
-            self.set_weights(self.initial_weights)
-            del self.initial_weights
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.units * 3,), name='bias', initializer=self.bias_initializer,
+                                        regularizer=self.bias_regularizer, constraint=self.bias_constraint)
+        else:
+            self.bias = None
+
+        self.kernel_z = self.kernel[:, :self.units]
+        self.recurrent_kernel_z = self.recurrent_kernel[:, :self.units]
+        self.kernel_r = self.kernel[:, self.units: self.units * 2]
+        self.recurrent_kernel_r = self.recurrent_kernel[:, self.units:
+        self.units * 2]
+        self.kernel_h = self.kernel[:, self.units * 2:]
+        self.recurrent_kernel_h = self.recurrent_kernel[:, self.units * 2:]
+
+        if self.use_bias:
+            self.bias_z = self.bias[:self.units]
+            self.bias_r = self.bias[self.units: self.units * 2]
+            self.bias_h = self.bias[self.units * 2:]
+        else:
+            self.bias_z = None
+            self.bias_r = None
+            self.bias_h = None
         self.built = True
 
-    def reset_states(self):
-        assert self.stateful, 'Layer must be stateful.'
-        input_shape = self.input_spec[0].shape
-        if not input_shape[0]:
-            raise ValueError('If a RNN is stateful, a complete '
-                             'input_shape must be provided '
-                             '(including batch size).')
-        if hasattr(self, 'states'):
-            K.set_value(self.states[0], np.zeros((input_shape[0], self.output_dim)))
-        else:
-            self.states = [K.zeros((input_shape[0], self.output_dim))]
-
-    def preprocess_input(self, x):
-        if self.consume_less == 'cpu':
-            input_shape = K.int_shape(x)
+    def preprocess_input(self, inputs, training=None):
+        if self.implementation == 0:
+            input_shape = K.int_shape(inputs)
             input_dim = input_shape[2]
             timesteps = input_shape[1]
 
-            x_z = time_distributed_dense(x, self.W_z, self.b_z, self.dropout_W, input_dim, self.output_dim, timesteps)
-            x_r = time_distributed_dense(x, self.W_r, self.b_r, self.dropout_W, input_dim, self.output_dim, timesteps)
-            x_h = time_distributed_dense(x, self.W_h, self.b_h, self.dropout_W, input_dim, self.output_dim, timesteps)
+            x_z = _time_distributed_dense(inputs, self.kernel_z, self.bias_z, self.dropout, input_dim, self.units,
+                                          timesteps, training=training)
+            x_r = _time_distributed_dense(inputs, self.kernel_r, self.bias_r, self.dropout, input_dim, self.units,
+                                          timesteps, training=training)
+            x_h = _time_distributed_dense(inputs, self.kernel_h, self.bias_h, self.dropout, input_dim, self.units,
+                                          timesteps, training=training)
             return K.concatenate([x_z, x_r, x_h], axis=2)
         else:
-            return x
+            return inputs
 
-    def step(self, x, states):
-        h_tm1 = states[0]  # previous memory
-        B_U = states[1]  # dropout matrices for recurrent units
-        B_W = states[2]
-
-        if self.consume_less == 'gpu':
-
-            matrix_x = K.dot(x * B_W[0], self.W) + self.b
-            matrix_inner = K.dot(h_tm1 * B_U[0], self.U[:, :2 * self.output_dim])
-
-            x_z = matrix_x[:, :self.output_dim]
-            x_r = matrix_x[:, self.output_dim: 2 * self.output_dim]
-            inner_z = matrix_inner[:, :self.output_dim]
-            inner_r = matrix_inner[:, self.output_dim: 2 * self.output_dim]
-
-            z = self.inner_activation(x_z + inner_z)
-            r = self.inner_activation(x_r + inner_r)
-
-            x_h = matrix_x[:, 2 * self.output_dim:]
-            inner_h = K.dot(r * h_tm1 * B_U[0], self.U[:, 2 * self.output_dim:])
-            hh = self.activation(x_h + inner_h)
-        else:
-            if self.consume_less == 'cpu':
-                x_z = x[:, :self.output_dim]
-                x_r = x[:, self.output_dim: 2 * self.output_dim]
-                x_h = x[:, 2 * self.output_dim:]
-            elif self.consume_less == 'mem':
-                x_z = K.dot(x * B_W[0], self.W_z) + self.b_z
-                x_r = K.dot(x * B_W[1], self.W_r) + self.b_r
-                x_h = K.dot(x * B_W[2], self.W_h) + self.b_h
-            else:
-                raise ValueError('Unknown `consume_less` mode.')
-            z = self.inner_activation(x_z + K.dot(h_tm1 * B_U[0], self.U_z))
-            r = self.inner_activation(x_r + K.dot(h_tm1 * B_U[1], self.U_r))
-
-            hh = self.activation(x_h + K.dot(r * h_tm1 * B_U[2], self.U_h))
-        h = z * h_tm1 + (1 - z) * hh
-        return h, [h]
-
-    def get_constants(self, x):
+    def get_constants(self, inputs, training=None):
         constants = []
-        if 0 < self.dropout_U < 1:
-            ones = K.ones_like(K.reshape(x[:, 0, 0], (-1, 1)))
-            ones = K.tile(ones, (1, self.output_dim))
-            B_U = [K.in_train_phase(K.dropout(ones, self.dropout_U), ones) for _ in range(3)]
-            constants.append(B_U)
+        if self.implementation != 0 and 0 < self.dropout < 1:
+            input_shape = K.int_shape(inputs)
+            input_dim = input_shape[-1]
+            ones = K.ones_like(K.reshape(inputs[:, 0, 0], (-1, 1)))
+            ones = K.tile(ones, (1, int(input_dim)))
+
+            def dropped_inputs():
+                return K.dropout(ones, self.dropout)
+
+            dp_mask = [K.in_train_phase(dropped_inputs, ones, training=training) for _ in range(3)]
+            constants.append(dp_mask)
         else:
             constants.append([K.cast_to_floatx(1.) for _ in range(3)])
 
-        if 0 < self.dropout_W < 1:
-            input_shape = K.int_shape(x)
-            input_dim = input_shape[-1]
-            ones = K.ones_like(K.reshape(x[:, 0, 0], (-1, 1)))
-            ones = K.tile(ones, (1, int(input_dim)))
-            B_W = [K.in_train_phase(K.dropout(ones, self.dropout_W), ones) for _ in range(3)]
-            constants.append(B_W)
+        if 0 < self.recurrent_dropout < 1:
+            ones = K.ones_like(K.reshape(inputs[:, 0, 0], (-1, 1)))
+            ones = K.tile(ones, (1, self.units))
+
+            def dropped_inputs():
+                return K.dropout(ones, self.recurrent_dropout)
+
+            rec_dp_mask = [K.in_train_phase(dropped_inputs, ones, training=training) for _ in range(3)]
+            constants.append(rec_dp_mask)
         else:
             constants.append([K.cast_to_floatx(1.) for _ in range(3)])
         return constants
 
+    def step(self, inputs, states):
+        h_tm1 = states[0]  # previous memory
+        dp_mask = states[1]  # dropout matrices for recurrent units
+        rec_dp_mask = states[2]
+
+        if self.implementation == 2:
+            matrix_x = K.dot(inputs * dp_mask[0], self.kernel)
+            if self.use_bias:
+                matrix_x = K.bias_add(matrix_x, self.bias)
+            matrix_inner = K.dot(h_tm1 * rec_dp_mask[0], self.recurrent_kernel[:, :2 * self.units])
+
+            x_z = matrix_x[:, :self.units]
+            x_r = matrix_x[:, self.units: 2 * self.units]
+            recurrent_z = matrix_inner[:, :self.units]
+            recurrent_r = matrix_inner[:, self.units: 2 * self.units]
+
+            z = self.recurrent_activation(x_z + recurrent_z)
+            r = self.recurrent_activation(x_r + recurrent_r)
+
+            x_h = matrix_x[:, 2 * self.units:]
+            recurrent_h = K.dot(r * h_tm1 * rec_dp_mask[0], self.recurrent_kernel[:, 2 * self.units:])
+            hh = self.activation(x_h + recurrent_h)
+        else:
+            if self.implementation == 0:
+                x_z = inputs[:, :self.units]
+                x_r = inputs[:, self.units: 2 * self.units]
+                x_h = inputs[:, 2 * self.units:]
+            elif self.implementation == 1:
+                x_z = K.dot(inputs * dp_mask[0], self.kernel_z)
+                x_r = K.dot(inputs * dp_mask[1], self.kernel_r)
+                x_h = K.dot(inputs * dp_mask[2], self.kernel_h)
+                if self.use_bias:
+                    x_z = K.bias_add(x_z, self.bias_z)
+                    x_r = K.bias_add(x_r, self.bias_r)
+                    x_h = K.bias_add(x_h, self.bias_h)
+            else:
+                raise ValueError('Unknown `implementation` mode.')
+            z = self.recurrent_activation(x_z + K.dot(h_tm1 * rec_dp_mask[0], self.recurrent_kernel_z))
+            r = self.recurrent_activation(x_r + K.dot(h_tm1 * rec_dp_mask[1], self.recurrent_kernel_r))
+
+            hh = self.activation(x_h + K.dot(r * h_tm1 * rec_dp_mask[2], self.recurrent_kernel_h))
+        h = z * h_tm1 + (1 - z) * hh
+        if 0 < self.dropout + self.recurrent_dropout:
+            h._uses_learning_phase = True
+        return h, [h]
+
     def get_config(self):
-        config = {'output_dim': self.output_dim, 'inner_init': self.inner_init.__name__,
-                  'inner_activation': self.inner_activation.__name__,
-                  'W_regularizer': self.W_regularizer.get_config() if self.W_regularizer else None,
-                  'U_regularizer': self.U_regularizer.get_config() if self.U_regularizer else None,
-                  'b_regularizer': self.b_regularizer.get_config() if self.b_regularizer else None,
-                  'dropout_W': self.dropout_W, 'dropout_U': self.dropout_U}
+        config = {'units': self.units, 'recurrent_activation': activations.serialize(self.recurrent_activation),
+                  'use_bias': self.use_bias, 'kernel_initializer': initializers.serialize(self.kernel_initializer),
+                  'recurrent_initializer': initializers.serialize(self.recurrent_initializer),
+                  'bias_initializer': initializers.serialize(self.bias_initializer),
+                  'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+                  'recurrent_regularizer': regularizers.serialize(self.recurrent_regularizer),
+                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+                  'activity_regularizer': regularizers.serialize(self.activity_regularizer),
+                  'kernel_constraint': constraints.serialize(self.kernel_constraint),
+                  'recurrent_constraint': constraints.serialize(self.recurrent_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint), 'dropout': self.dropout,
+                  'recurrent_dropout': self.recurrent_dropout}
         base_config = super(ELUGRU, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
--- src/models.py
+++ src/models.py
@@ -1,8 +1,7 @@
 from keras.engine import Input
 from keras.engine import Model
-from keras.layers import merge
-from keras.regularizers import WeightRegularizer
-from keras.layers import Embedding, TimeDistributed, Dense, Dropout, ELU, Bidirectional
+from keras.regularizers import L1L2
+from keras.layers import Embedding, TimeDistributed, Dense, Dropout, ELU, Bidirectional, concatenate
 from keras.optimizers import Adam
 
 from custom_layers import BetterTimeDistributed, ELUGRU
@@ -10,25 +9,23 @@ from custom_layers import BetterTimeDistributed, ELUGRU
 
 def bielugru(word_input_size, word_embedding_size, sequence_embedding_size, n_tags, word_dropout, rnn_dropout_W,
              rnn_dropout_U, l2, embedding_weights=None, **kwargs):
-    default_initialization = "he_normal"
-    default_activation = ELU
-
     text_input = Input(shape=(None,), dtype='int32', name='text_input')
 
     word_embeddings = Embedding(input_dim=word_input_size, output_dim=word_embedding_size, weights=embedding_weights,
-                                dropout=word_dropout, name="word_embeddings")(text_input)
-
+                                name="word_embeddings")(text_input)
+    word_embeddings = Dropout(word_dropout)(word_embeddings)
     sequence_embedding = word_embeddings
     # TEXT RNN
 
     bidirectional_tag_sequence_output = Bidirectional(
-        ELUGRU(output_dim=sequence_embedding_size / 2, return_sequences=True, init=default_initialization,
-               dropout_W=rnn_dropout_W, dropout_U=rnn_dropout_U), merge_mode="concat")(sequence_embedding)
+        ELUGRU(sequence_embedding_size / 2, return_sequences=True, dropout=rnn_dropout_W,
+               recurrent_dropout=rnn_dropout_U), merge_mode="concat")(sequence_embedding)
 
-    tag_sequence_output = TimeDistributed(Dense(n_tags, activation='softmax', W_regularizer=WeightRegularizer(l2=l2)),
+    tag_sequence_output = TimeDistributed(Dense(n_tags, activation='softmax', kernel_regularizer=L1L2(l2=l2)),
                                           name="aspect_output")(bidirectional_tag_sequence_output)
-    model = Model(input=[text_input], output=[tag_sequence_output])
-    adam = Adam(clipnorm=5.0)
+    model = Model(inputs=[text_input], outputs=[tag_sequence_output])
+    adam = Adam()
+    # adam = Adam(clipnorm=5.0)
     model.compile(optimizer=adam, loss={'aspect_output': "categorical_crossentropy"}, sample_weight_mode="temporal")
     model._make_train_function()
     model._make_predict_function()
@@ -37,43 +34,41 @@ def bielugru(word_input_size, word_embedding_size, sequence_embedding_size, n_ta
 
 def char_bielugru(word_input_size, word_embedding_size, char_input_size, char_embedding_size, sequence_embedding_size,
                   n_tags, word_dropout, rnn_dropout_W, rnn_dropout_U, dropout, l2, embedding_weights=None, **kwargs):
-    default_initialization = "he_normal"
-    default_activation = ELU
-
     text_input = Input(shape=(None,), dtype='int32', name='text_input')
     char_input = Input(shape=(None, None,), dtype='int32', name='char_input')
 
     char_word_input = Input(shape=(None,), dtype='int32', name='char_word_input')
 
     word_embeddings = Embedding(input_dim=word_input_size, output_dim=word_embedding_size, weights=embedding_weights,
-                                dropout=word_dropout, name="word_embeddings")(text_input)
+                                name="word_embeddings")(text_input)
+    word_embeddings = Dropout(word_dropout)(word_embeddings)
 
-    char_embedding_layer = Embedding(input_dim=char_input_size, output_dim=char_embedding_size, dropout=word_dropout,
-                                     name="char_embeddings")
+    char_embedding_layer = Embedding(input_dim=char_input_size, output_dim=char_embedding_size, name="char_embeddings")
     char_embeddings = BetterTimeDistributed(char_embedding_layer)(char_input)
-
+    char_embeddings = Dropout(word_dropout)(char_embeddings)
     ##################
     ##################
-    char_word_model = Bidirectional(ELUGRU(output_dim=char_embedding_size, return_sequences=False), merge_mode="concat")
+    char_word_model = Bidirectional(ELUGRU(char_embedding_size, return_sequences=False), merge_mode="concat")
 
     char_word_embeddings = BetterTimeDistributed(char_word_model)(char_embeddings)
     char_word_embeddings = Dropout(dropout)(char_word_embeddings)
-    char_word_embeddings = BetterTimeDistributed(Dense(char_embedding_size, W_regularizer=WeightRegularizer(l2=l2)))(
+    char_word_embeddings = BetterTimeDistributed(Dense(char_embedding_size, kernel_regularizer=L1L2(l2=l2)))(
         char_word_embeddings)
     ##################
     ##################
 
-    sequence_embedding = merge([word_embeddings, char_word_embeddings])
+    sequence_embedding = concatenate([word_embeddings, char_word_embeddings])
     # TEXT RNN
 
     bidirectional_tag_sequence_output = Bidirectional(
-        ELUGRU(output_dim=sequence_embedding_size / 2, return_sequences=True, init=default_initialization,
-               dropout_W=rnn_dropout_W, dropout_U=rnn_dropout_U), merge_mode="concat")(sequence_embedding)
+        ELUGRU(sequence_embedding_size / 2, return_sequences=True, dropout=rnn_dropout_W,
+               recurrent_dropout=rnn_dropout_U), merge_mode="concat")(sequence_embedding)
 
-    tag_sequence_output = TimeDistributed(Dense(n_tags, activation='softmax', W_regularizer=WeightRegularizer(l2=l2)),
+    tag_sequence_output = TimeDistributed(Dense(n_tags, activation='softmax', kernel_regularizer=L1L2(l2=l2)),
                                           name="aspect_output")(bidirectional_tag_sequence_output)
-    model = Model(input=[text_input, char_input], output=[tag_sequence_output])
-    adam = Adam(clipnorm=5.0)
+    model = Model(inputs=[text_input, char_input], outputs=[tag_sequence_output])
+    adam = Adam()
+    # adam = Adam(clipnorm=5.0)
     model.compile(optimizer=adam, loss={'aspect_output': "categorical_crossentropy"}, sample_weight_mode="temporal")
     model._make_train_function()
     model._make_predict_function()
